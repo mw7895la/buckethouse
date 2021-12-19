ba3edde9fc177b5e7df9ad4d14ef88dd7d2698fb
legendmic2 <goonoo0731@gmail.com>
2021-12-19 15:54:11 +0900

    Initial commit

---
+++ src/main/java/com/clone/buckethouse/BuckethouseApplication.java
@@ -0,0 +1,13 @@
+package com.clone.buckethouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuckethouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuckethouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clone/buckethouse/BuckethouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.buckethouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuckethouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
