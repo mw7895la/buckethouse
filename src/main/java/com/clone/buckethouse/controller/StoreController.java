3e501f460320f224937b2995957783e25a300856
legendmic2 <goonoo0731@gmail.com>
2022-01-05 20:44:47 +0900

    [Test] git branch merge test

--- src/main/java/com/clone/buckethouse/controller/StoreController.java
+++ src/main/java/com/clone/buckethouse/controller/StoreController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -98,6 +97,7 @@ public class StoreController {
     public ResponseEntity<?> deleteStoreContent(@RequestBody StoreContentDTO dto){
         try{
             String temporaryUserId = "temporary-user"; // temporary user id.
+            // Test
 
             // (1) ProductEntity로 변환한다.
             ProductEntity entity = StoreContentDTO.toEntity(dto);
